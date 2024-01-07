0ff4afccc5487480e3bab22a1f73d58068dbc548
gathuitapius <gathuitapius097@gmail.com>
2024-01-07 06:57:39 -0500

    develop a Python web app using Flask and integrate Embeddable Watson AI libraries to make the web app showcase AI-based abilities

---
+++ server.py
@@ -0,0 +1,33 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import Flask, request, render_template
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+app = Flask("Sentiment Analysis using BERT")
+
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs sentiment analysis over it using sentiment_analysis()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    ''' This functions executes the flask app and deploys it on localhost:5000
+    '''
+    app.run(host= "0.0.0.0", port=5001)
